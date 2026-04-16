32a5a0709a68e13e9dd5016ce76bf8ae30b89765
kayal <kayalelango01@gmail.com>
2026-04-16 18:49:26 +0530

    auto create demo user on startup

--- build.sh
+++ build.sh
@@ -4,3 +4,14 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+if not User.objects.filter(username='demo').exists():
+    user = User.objects.create_user(username='demo', password='demo1234', email='demo@mindbloom.com')
+    Token.objects.get_or_create(user=user)
+    print('Demo user created!')
+else:
+    print('Demo user already exists.')
+"
